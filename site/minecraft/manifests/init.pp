870471a3f98de1726e4b3ff02d143305d855e5b2
superawesome7070 <46094482+superawesome7070@users.noreply.github.com>
2020-03-21 16:59:49 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,10 +1,12 @@
 class minecraft {
-  file {'/opt/minecraft':
+$url = 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar'
+$install_dir = '/opt/minecraft'
+  file {"${install_dir}/opt/minecraft":
     ensure => directory,
  }
- file {'/opt/minecraft/server.jar':
+ file {'/server.jar':
     ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
+    source => $url,
     before => Service['minecraft'],
  }
  package {'java':
